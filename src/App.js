0b7f017aec94d1e50d16d6a7136b8439eb5ac65c
Haris Berilo <beriloharis@gmail.com>
2022-05-26 23:00:23 +0200

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -28,45 +28,63 @@ function App() {
   
   return (
     <>
-			<section className="title-container w-screen my-5 mx-5 xl:mx-10">
-				<div className="title h-10">
-          			logo
-        		</div>
+			<section className="mx-auto w-11/12 my-5">
+
+				<div className="title h-10 w-full">logo</div>
+
 			</section>
 
-			<section className="flex flex-col lg:flex-row post-container h-100 px-5 mb-10 xl:px-10 w-full lg:justify-between">
-				<div className="hidden lg:flex lg:flex-column ads lg:ads-height lg:w-48 xl:w-72">
+
+
+			<section className="flex flex-col mx-auto lg:flex-row h-100 mb-10 w-11/12 lg:justify-between">
+
+				<div className="hidden lg:flex lg:flex-column ads lg:ads-height w-2/12">
+
 					ads/reccs
+
 				</div>
 
-				<div className="flex flex-col posts w-full mx-auto">
-					<div className="post post-height w-full mb-5">
-            			main screen
-          			</div>
-					<div className="like like-height w-full mb-10">
-						main title
-					</div>
-				</div>	
 
-      <dir className="recommendations w-full p-0 flex flex-row lg:flex-col flex-wrap lg:flex-nowrap justify-between mx-auto lg:mx-0 lg:mr-10">
-        <div className="reccs w-full sm:w-5/12 lg:w-full h-40 lg:h-24 bg-white mb-10">
-          recommendation
-        </div>  
-        <div className="reccs w-full sm:w-5/12 lg:w-full h-40 lg:h-24 bg-white mb-10">
-          recommendation
-        </div>
-        <div className="reccs w-full sm:w-5/12 lg:w-full h-40 lg:h-24 bg-white mb-10">
-          recommendation
-        </div>
-        <div className="reccs w-full sm:w-5/12 lg:w-full h-40 lg:h-24 bg-white mb-10">
-          recommendation
-        </div>
-      </dir>        
-        
-    
-        
-       
-      		</section>
+
+				<div className="posts mx-auto w-11/12 lg:w-8/12">
+
+					<div className="post post-height lg:w-10/12 mb-5 mx-auto">main screen</div>
+
+					<div className="like like-height lg:w-10/12 mb-10 mx-auto">main title</div>
+
+				</div>
+
+
+
+				<div className="recommendations w-8/12 sm:w-11/12 lg:w-2/12 p-0 flex flex-row lg:flex-col flex-wrap lg:flex-nowrap justify-between mx-auto lg:mx-0">
+
+					<div className="reccs w-full sm:w-5/12 lg:w-full h-40 lg:h-24 bg-white mb-10">
+
+						recommendation
+
+					</div>
+
+					<div className="reccs w-full sm:w-5/12 lg:w-full h-40 lg:h-24 bg-white mb-10">
+
+						recommendation
+
+					</div>
+
+					<div className="reccs w-full sm:w-5/12 lg:w-full h-40 lg:h-24 bg-white mb-10">
+
+						recommendation
+
+					</div>
+
+					<div className="reccs w-full sm:w-5/12 lg:w-full h-40 lg:h-24 bg-white mb-10">
+
+						recommendation
+
+					</div>
+
+				</div>
+
+			</section>
 		</>
   );
 }
